eed17a45d01d0423b4b9a61fcb56e394c2c7c055
darksystem <diogo.rey@hotmail.com>
2019-04-05 13:48:45 +0100

    Refactor on scenario3

--- src/queue/Ventilator.ts
+++ src/queue/Ventilator.ts
@@ -33,31 +33,3 @@ export class Subscriber implements Observable {
         console.log("Subscriber id " + this.id + " Received the message : " + message)
     }
 }
-
-// Creating Ventilator
-let queue = new Queue.BoundedAsyncQueue<string>(1);
-const ventilator = new Ventilator()
-
-//Publisher throwing messages
-setTimeout(async function() {
-    await queue.push('first');
-    await queue.push('second');
-}, 1000);
-
-// Creating Subscribers
-const subscriberA = new Subscriber(1)
-const subscriberB = new Subscriber(2)
-const subscriberC = new Subscriber(3)
-
-// Subscribing to the Ventilator
-ventilator.addObserver(subscriberA)
-ventilator.addObserver(subscriberB)
-ventilator.addObserver(subscriberC);
-
-
-(async () => {
-    // await message
-    // Notify all of our subscribers about the messages
-    ventilator.notifyObservers(queue.pop())
-    ventilator.notifyObservers(queue.pop())
-})()
--- src/scenario3.ts
+++ src/scenario3.ts
@@ -12,6 +12,38 @@ Manages message meta-information, such as marking them as tentatively consumed, 
 
 */
 
+import {Queue} from "./queue/api";
+import {Subscriber, Ventilator} from './queue/Ventilator'
+
 export function testScenarioThree() {
-    // TODO
+
+    // Creating Ventilator
+    let queue = new Queue.BoundedAsyncQueue<string>(1);
+    const ventilator = new Ventilator()
+
+    //Publisher throwing messages
+    setTimeout(async function() {
+        await queue.push('first');
+        await queue.push('second');
+    }, 1000);
+
+    // Creating Subscribers
+    const subscriberA = new Subscriber(1)
+    const subscriberB = new Subscriber(2)
+    const subscriberC = new Subscriber(3)
+
+    // Subscribing to the Ventilator
+    ventilator.addObserver(subscriberA)
+    ventilator.addObserver(subscriberB)
+    ventilator.addObserver(subscriberC);
+
+
+    (async () => {
+        // await message
+        // Notify all of our subscribers about the messages
+        ventilator.notifyObservers(queue.pop())
+        ventilator.notifyObservers(queue.pop())
+    })()
 }
+
+testScenarioThree();
