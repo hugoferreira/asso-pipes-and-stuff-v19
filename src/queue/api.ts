3bddc6f762d558bd6bf2ab5e96efc19b59dda6e1
AndreFCruz <andrecruz97@gmail.com>
2019-04-06 12:48:01 +0100

    implemented BoundedQueue without semaphore

--- src/queue/api.ts
+++ src/queue/api.ts
@@ -2,6 +2,7 @@ import { BlockingQueue as _BlockingQueue } from './BlockingQueue'
 import { AsyncQueue as _AsyncQueue } from './UnboundedAsyncQueue'
 import { UnboundedQueue as _UnboundedQueue } from './UnboundedAsyncQueue'
 import { BoundedQueue as _BoundedQueue } from './BoundedAsyncQueue'
+import { BoundedAsyncQueue as _BoundedAsyncQueue } from './BoundedAsyncQueue'
 
 export namespace Queue {
 
@@ -11,10 +12,13 @@ export namespace Queue {
     export const AsyncQueue = _AsyncQueue;
     export type AsyncQueue<T> = _AsyncQueue<T>;
 
+    export const UnboundedQueue = _UnboundedQueue;
+    export type UnboundedQueue<T> = _UnboundedQueue<T>;
+
     export const BoundedQueue = _BoundedQueue;
     export type BoundedQueue<T> = _BoundedQueue<T>;
 
-    export const UnboundedQueue = _UnboundedQueue;
-    export type UnboundedQueue<T> = _UnboundedQueue<T>;
+    export const BoundedAsyncQueue = _BoundedAsyncQueue;
+    export type BoundedAsyncQueue<T> = _BoundedAsyncQueue<T>;
 
 }
